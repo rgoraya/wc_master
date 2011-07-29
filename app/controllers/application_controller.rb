ba2b617c61805892ae98f7f1228054eceed4c2bf
nguyendl <duyetln@yahoo.com>
2011-07-29 16:14:59 -0700

    Fixed that 'record' thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   
   def current_user
     return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
+    @current_user = current_user_session && current_user_session.user
   end
 
 end
