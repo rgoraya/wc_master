2c0dc5875ed8b82839ec4921a761d3a6f68730d5
rgoraya <rgoraya@uci.edu>
2011-10-09 12:23:48 -0700

    resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -83,81 +83,3 @@ ActiveRecord::Schema.define(:version => 20110930102036) do
   add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
-=======
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20110827075743) do
-
-  create_table "issues", :force => true do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "wiki_url"
-    t.string   "short_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "permalink"
-    t.integer  "user_id"
-  end
-
-  create_table "references", :force => true do |t|
-    t.integer  "relationship_id"
-    t.string   "reference_content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
-
-  create_table "relationships", :force => true do |t|
-    t.integer  "issue_id"
-    t.integer  "cause_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.string   "relationship_type"
-  end
-
-  create_table "suggestions", :force => true do |t|
-    t.string   "title"
-    t.string   "wiki_url"
-    t.string   "causality"
-    t.string   "status"
-    t.integer  "issue_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "crypted_password"
-    t.string   "password_salt"
-    t.string   "persistence_token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reputation",        :default => 1
-  end
-
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",     :null => false
-    t.integer  "item_id",       :null => false
-    t.string   "event",         :null => false
-    t.string   "whodunnit"
-    t.text     "object"
-    t.datetime "created_at"
-    t.integer  "reverted_from"
-  end
-
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
-
-end
->>>>>>> 3fea20928d74f82d54f63aaa61ad184d4aa4d47f
