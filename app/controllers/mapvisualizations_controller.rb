1b15650a04b8643ecf06d1ee13bf2f3f2be6d661
highbulp <highbulp@gmail.com>
2012-01-12 15:43:04 -0800

    moved visualization data structure creation into ruby; data is imported into javascript where it can be accessed by causemap_rjs:

--- app/controllers/mapvisualizations_controller.rb
+++ app/controllers/mapvisualizations_controller.rb
@@ -1,33 +1,111 @@
+require 'matrix'
+
 class MapvisualizationsController < ApplicationController
-  # GET /mapvisualizations
-  # GET /mapvisualizations.xml
-  def index
-    @mapvisualizations = Mapvisualization.all
 
-    # do method calls / variable declarations here
+######## BEGIN CLASS DEFINITIONS #########
+  ## Data structures for easier use 
+  class Node < Object
+    attr_accessor :id, :name, :weight, :location
 
-    @my_var = 12 #testing variable
-     
-
-
-
-
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @mapvisualizations }
+    def initialize(id, name, weight)
+      @id = id #needed? currently using index as an id; may need to tweak this as we start fetching from db
+      @name = name
+      @weight = weight
+      @location = Vector[0,0]
     end
+    
+    def to_s
+      @name.to_s + "("+@location.to_s+")"
+    end
+
+    #returns a javascript version of the object
+    def js
+      "{name:'"+@name+"',"+
+      "x:"+@location[0].to_s+",y:"+@location[1].to_s+","+
+      "weight:"+@weight.to_s+"}" 
+      #can add more fields as needed
+    end
+    
+  end  
+
+  class Edge < Object
+    attr_accessor :id, :a, :b, :weight, :type
+    
+    def initialize(id, a, b, weight)
+      @id = id #needed?
+      @a = a #reference to the node object (as opposed to just an index)
+      @b = b
+      @weight = weight
+      @type = 0
+    end
+  
+    def to_s
+      @id.to_s+": Edge "+@a.to_s+" - "+@b.to_s
+    end
+    
+    def name
+      "Edge "+@a.name+" - "+@b.name
+    end
+
+    #returns a javascript version of the object 
+    #ai and bi are the js indices for the connecting nodes (default to node's ID)
+    #nodeset is the name of the js node array (default to "nodes")
+    def js(ai=@a.id, bi=@b.id, nodeset='nodes') 
+      "{name:'"+name+"',"+
+      "a:"+nodeset+"["+ai.to_s+"],b:"+nodeset+"["+bi.to_s+"]"+","+
+      "weight:"+@weight.to_s+"}"
+      #can add more fields as needed
+    end
+
+  end
+  
+######## END CLASS DEFINITIONS #########  
+
+  # the code run on index load
+  # GET /mapvisualizations
+  def index
+    # @mapvisualizations = Mapvisualization.all
+    node_count = 10
+
+    @nodes = Array.new(node_count) {|i| Node.new(i, "Node "+i.to_s, rand()*5)} #make all the nodes (random)
+    @edges = Array.new() #an array to hold edges
+    @adjacency = Array.new(node_count) {|i| Array.new(node_count)} #an adjacency matrix of edges (for easy referencing)
+    for i in (0..node_count-1)
+      for j in (i+1..node_count-1)
+        if(rand() > 0.8) #make random edges
+          @edges.push(Edge.new(j*node_count+i, @nodes[i], @nodes[j], rand()*5))
+          @adjacency[i][j] = @edges.last
+          @adjacency[j][i] = @edges.last
+        end
+      end
+    end
+    
+    circle_nodes(@nodes, 500, 500) #default circle nodes in 500x500 canvas? This method should be elsewhere...
+  
   end
 
 
-#DEFINE NEW METHOD CALLS IF NEEEDED
+  # put the given nodes into a circle that will (mostly) fit in the given canvas
+  def circle_nodes(nodeset, width, height)
+    center = Vector[width/2, height/2]
+    radius = 0.375*[width,height].min
+    
+    nodeset.each_index{|i| nodeset[i].location = Vector[
+      center[0] + (radius * Math.cos(2*Math::PI*i/nodeset.length)), 
+      center[1] + (radius * Math.sin(2*Math::PI*i/nodeset.length))]}
+  end
 
+  def force_layout(nodeset, width, height)
+    #put the nodes in a force-based layout
+  end
 
+  
 
 
 
 ############################################################
-
+### OLD RESTFUL METHODS
+############################################################
 
   # GET /mapvisualizations/1
   # GET /mapvisualizations/1.xml
--- public/javascripts/causemap_rjs.js
+++ public/javascripts/causemap_rjs.js
@@ -2,35 +2,11 @@
 var paper; //so can draw in other methods
 window.onload = function(){
 	paper = new Raphael(document.getElementById('canvas_container'), 500, 500) //graphics context
-
-  //set locations for the nodes; do it here so we can tweak based on display size??
-  //draw nodes into evenly-spaced rows
-  // num_cols = Math.ceil(Math.sqrt(nodes.length+1));
-  // col_len = Math.floor(paper.width/num_cols);
-  // num_rows = Math.ceil((nodes.length+1)/num_cols);
-  // row_len = Math.floor(paper.height/num_rows);
-  // for(var i=0; i<nodes.length; i++)
-  // {
-  //   nodes[i].x = (.5 + (i%num_cols))*col_len; //var row = Math.floor(i/num_cols);
-  //   nodes[i].y = (.5 + Math.floor(i/num_cols))*row_len; //var col = i % num_cols;
-  // }
-
-  //initial arrange in circle, radius = 200, centered at 250,250
-  for(var i=0; i<nodes.length; i++)
-  {
-    nodes[i].x = 250 + (200 * Math.cos(2*Math.PI*i/nodes.length));
-    nodes[i].y = 250 + (200 * Math.sin(2*Math.PI*i/nodes.length));
-  }
-  
-  drawNodes()
-
-  /*****************
-  ******************AFTER WE HAVE LOADED, DEFINE FUNCTIONS WE'LL NEED
-  ******************/
+  drawNodes() //call draw on the nodes
 }
 
 
-function drawNodes()
+function drawNodes() //outside of onload so that we can redraw
 {
   paper.clear() //clear out old drawings
   //draw nodes
@@ -53,76 +29,19 @@ function drawNodes()
 
   for(var i=0; i<edges.length; i++)
   {
-    var a = nodes[edges[i].a]
-    var b = nodes[edges[i].b]
+    var a = edges[i].a //for quick access
+    var b = edges[i].b
+
     var e = paper.path("M"+a.x+","+a.y+"L"+b.x+","+b.y+"z").toBack().attr({
       stroke: '#bd55dd',
       'stroke-width': edges[i].weight
     })
-    .data("name","Edge from "+a.name+" to "+ b.name)
+    .data("name",edges[i].name)
     .click(function() { alert("You clicked on "+this.data("name"))})
     .mouseover(function() {this.node.style.cursor='pointer';})
 
   }  
 }
-
-function whichclick(item)
-{
-  alert("you clicked on"+item)
-}
-
-function eadesLayout()
-{
-  //ORGANIZE NODES (SPRING SYSTEM)
-  //start with implementing kamada-kawai
-  //the try fruchterman-reingold; can combine if needed (and have render time)
-    //note that for very large graphs, this code should be moved server-side (we should probably get positions before we draw?)
-
-  c1 = 2; c2 = 1; c3 = 1; c4 = 0.1;
-  for(var m=0; m<100; m++)
-  {
-    for(var i=0; i<nodes.length; i++)
-    {
-      nodes[i].dx = 0; nodes[i].dy = 0;
-      for(var j=0; j<nodes.length; j++)
-      {
-        if(i!=j)
-        {
-          dist = node_dist(nodes[i], nodes[j])
-          nx = (nodes[j].x-nodes[i].x)/dist //normal vector
-          ny = (nodes[j].y-nodes[i].y)/dist
-          if(adjacency[i][j] == 1) //if connected according to adjacency matrix
-          {
-            f = c1*Math.log(dist/c2)/Math.LN10 //force
-            if(i==0 && j==1)
-              console.log("attract:"+f);
-            nodes[i].dx += f*nx //move along attracting vector
-            nodes[i].dy += f*ny
-          }
-          else
-          {
-            f = c3/Math.sqrt(dist)
-            if(i==0 && j==1)
-              console.log("repel:"+f)
-            nodes[i].dx -= f*nx //move away from repelling vector
-            nodes[i].dy -= f*ny
-          }
-        }
-      }
-    }
-    console.log("Node 0 moved:"+nodes[0].dx+", "+nodes[0].dy)
-    for(var i=0; i<nodes.length; i++)
-    {
-      nodes[i].x += c4*nodes[i].dx //move along vector
-      nodes[i].y += c4*nodes[i].dy
-    }
-    //DRAW THE NODES TO DEBUG!!
-
-    drawNodes();
-
-  }
-}
-
 
 
 
