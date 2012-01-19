489d47ffc4d574e0dad6c5f835d28bddb3cba296
highbulp <highbulp@gmail.com>
2012-01-18 16:31:55 -0800

    now with all the files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120112003138) do
+ActiveRecord::Schema.define(:version => 20120118172502) do
 
   create_table "feed_backs", :force => true do |t|
     t.string   "subject"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(:version => 20120112003138) do
 
   add_index "relationships", ["cause_id"], :name => "index_relationships_on_cause_id"
   add_index "relationships", ["issue_id"], :name => "index_relationships_on_issue_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "suggestions", :force => true do |t|
     t.string   "title"
