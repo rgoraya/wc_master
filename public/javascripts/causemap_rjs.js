39e85552f1e03987c1be96f2479a6f4c0dd39da1
highbulp <highbulp@gmail.com>
2012-01-24 15:14:21 -0800

    added simple animations for altered layouts of same graph. Also added a spinner while ajax is running. Pushing before sizable javascript refacter from arrays to hashes for easier comparison/indexing, so that there is a working copy in the repo

--- public/javascripts/causemap_rjs.js
+++ public/javascripts/causemap_rjs.js
@@ -36,6 +36,57 @@ function drawNodes(nodes, edges, paper)
   }  
 }
 
+function animateNodes(fromNodes, fromEdges, toNodes, toEdges, paper)
+{
+  paper.clear() //start blank
+  easing = "backOut" //either this or linear
+  
+  for(var i=0; i<fromNodes.length; i++)
+  {
+    paper.circle(fromNodes[i].x, fromNodes[i].y, 10+(fromNodes[i].weight*6))
+    .attr({
+      fill: '#54b8dd', stroke: '#bebebe', 'stroke-width': 2
+    })
+    .data("name",fromNodes[i].name)
+    .click(function() { alert("You clicked on "+this.data("name"))})
+    .mouseover(function() {this.node.style.cursor = 'pointer';})
+    .animate({
+      cx: toNodes[i].x,
+      cy: toNodes[i].y
+    }, 1000, easing)
+
+    paper.text(fromNodes[i].x, fromNodes[i].y+25, fromNodes[i].name)
+    .data("name",fromNodes[i].name)
+    .click(function() { alert("You clicked on "+this.data("name"))})
+    .mouseover(function() {this.node.style.cursor='pointer';})
+    .animate({
+      x: toNodes[i].x,
+      y: toNodes[i].y+25
+    }, 1000, easing)
+    
+  }
+
+  for(var i=0; i<fromEdges.length; i++)
+  {
+    var fa = fromEdges[i].a //for quick access
+    var fb = fromEdges[i].b
+    var ta = toEdges[i].a
+    var tb = toEdges[i].b
+
+    paper.path("M"+fa.x+","+fa.y+"L"+fb.x+","+fb.y+"z").toBack().attr({
+      stroke: '#bd55dd',
+      'stroke-width': fromEdges[i].weight
+    })
+    .data("name",fromEdges[i].name)
+    .click(function() { alert("You clicked on "+this.data("name"))})
+    .mouseover(function() {this.node.style.cursor='pointer';})
+    .animate({
+      path: "M"+ta.x+","+ta.y+"L"+tb.x+","+tb.y+"z"
+    }, 1000, easing)
+
+  }  
+  
+}
 
 
 
