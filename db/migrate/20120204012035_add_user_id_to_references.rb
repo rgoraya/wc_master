f6a8b19d9eedaef99190a7eea27bd3db46808e73
nguyendl <duyetln@yahoo.com>
2012-02-07 11:08:54 -0800

    remove conflicting migrations

--- db/migrate/20120204012035_add_user_id_to_references.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdToReferences < ActiveRecord::Migration
-  def self.up
-    add_column :references, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :references, :user_id
-  end
-end
