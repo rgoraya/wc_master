17167a2828820b48f8745570229a862c263d403c
nguyendl <duyetln@yahoo.com>
2012-02-11 17:17:07 -0800

    update interface of versions page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -386,7 +386,6 @@ require 'backports'
     @issue = Issue.find(params[:id])
 		@versions = []
 		if Version.last.id - params[:more].to_i >= 0
-
 			Version.order("created_at DESC").find(:all, :conditions=>["id <= ? AND item_type = ?", Version.last.id - params[:more].to_i, "Relationship"]).each do |version|
 				relationship = version.get_object
 				if relationship.issue_id == @issue.id || relationship.cause_id == @issue.id
