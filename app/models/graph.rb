1749d83745ed9e78d4de606da2644cc3f512aa3a
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-17 06:44:05 -0800

    Created separate pathfinder model, updated graph class.

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,27 +1,47 @@
-require 'active_model'
-
 class Graph
 	include ActiveModel::Validations
 
 	validates_presence_of :nodes, :edges
 
+	# Initialization and Attributes
 	attr_accessor :nodes, :edges
 	def initialize(issues)
 		# Generates nodes from list of issues
-		@nodes = issues # To do: initialize with Node class
+		@nodes = [] # To do: initialize with Node class
 
 		# Retrieve relationships between issues
-		@edges = {}
+		@edges = [] # To do
 	end
 
 	def initialize
 		# Basic empty graph
-		@nodes = []
-		@edges = []
+		@nodes = [] # Initialize as empty list of node objects?
+		@edges = [] # Initialize as empty list of edges?
 	end
 
-	
+	# Custom graph generation
+	def get_graph_of_path(src, dest, limit)
+	end
 
+	def get_graph_of_effects(issue, steps, limit)
+	end
+
+	def get_graph_where (condition, limit)
+	end
+
+	def get_graph_of_most_recent (limit)
+	end
+	
+	def get_graph_of_most_connected (limit)
+    end
+
+	def get_graph_of_all
+	end
+
+	def get_graph_of_nas
+	end
+
+	# PathFinding
 end
 
 
---
+++ app/models/pathfinder.rb
@@ -0,0 +1,15 @@
+class PathFinder
+	include ActiveModel::Validations
+
+	validates_presence_of :source
+	attr_accessor :source, :destination
+	def initialize(source, destination=0)
+		@source = source
+		@destination = destination
+	end
+
+	# Placeholder for path algorithms here
+	# These will either return a single path or 
+	# a bunch of them, depending on API call
+	# Wanted to separate from graph logic to make it more modular.
+end
