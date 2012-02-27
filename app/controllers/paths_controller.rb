6b0e33360bfb805387b617b060d3cf1266de11f9
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-27 04:57:50 -0800

    Updated controller with demo check

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -3,10 +3,16 @@ class PathsController < ApplicationController
   # GET /paths.xml
   def index
 	@paths = Issue.order("created_at ASC").limit(40)
+	@src = 1
 	@g = Graph.new
 
-	
-	
+	if params[:demo] == 'all'
+
+	elsif params[:demo] == 'original'
+
+	elsif params[:demo] == 'recent'
+
+	end	
   end
 
 end
