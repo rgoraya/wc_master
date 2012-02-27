bc2d3d17c316cd5c3563b615d1368047336a0a62
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-27 05:14:16 -0800

    Demo now includes indication of which demo type.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -4,13 +4,17 @@ class PathsController < ApplicationController
   def index
 	@src = 1
 	@g = Graph.new
+	@demo_type = ""
 
 	### Demo ###
 	if params[:demo] == 'all'
+		@demo_type = "All Issues"
 		@g.demo_all()
 	elsif params[:demo] == 'original'
+		@demo_type = "Original Issues"
 		@g.get_graph_of_earliest()
 	elsif params[:demo] == 'recent'
+		@demo_type = "Most Recent Issues"
 		@g.get_graph_of_most_recent()
 	end	
   end
