3ee750a877be7986b3f9edc9b6bf4e04b4c6f22f
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-27 04:19:22 -0800

    Cleaned up paths testbed controller.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,82 +2,11 @@ class PathsController < ApplicationController
   # GET /paths
   # GET /paths.xml
   def index
-    @paths = Path.all
+    @paths = Issue.order("created_at DESC").paginate(:per_page => 20, :page => params[:page])
 
     respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @paths }
+      render :nothing => true
     end
   end
 
-  # GET /paths/1
-  # GET /paths/1.xml
-  def show
-    @path = Path.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @path }
-    end
-  end
-
-  # GET /paths/new
-  # GET /paths/new.xml
-  def new
-    @path = Path.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @path }
-    end
-  end
-
-  # GET /paths/1/edit
-  def edit
-    @path = Path.find(params[:id])
-  end
-
-  # POST /paths
-  # POST /paths.xml
-  def create
-    @path = Path.new(params[:path])
-
-    respond_to do |format|
-      if @path.save
-        format.html { redirect_to(@path, :notice => 'Path was successfully created.') }
-        format.xml  { render :xml => @path, :status => :created, :location => @path }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @path.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /paths/1
-  # PUT /paths/1.xml
-  def update
-    @path = Path.find(params[:id])
-
-    respond_to do |format|
-      if @path.update_attributes(params[:path])
-        format.html { redirect_to(@path, :notice => 'Path was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @path.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /paths/1
-  # DELETE /paths/1.xml
-  def destroy
-    @path = Path.find(params[:id])
-    @path.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(paths_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
