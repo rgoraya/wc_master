3ebcf8fd045462ffff3254b4158c74275da7af66
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-27 04:36:31 -0800

    Created a layout. It is not too pretty.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,11 +2,7 @@ class PathsController < ApplicationController
   # GET /paths
   # GET /paths.xml
   def index
-    @paths = Issue.order("created_at DESC").paginate(:per_page => 20, :page => params[:page])
-
-    respond_to do |format|
-      render :nothing => true
-    end
+  	@paths = Issue.order("created_at DESC").limit(40)
   end
 
 end
