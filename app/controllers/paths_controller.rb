32ae0c9513b97db02c8f7e00f3d468179fea2c95
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-02-27 05:00:54 -0800

    Paths Controller now calls demo methods on graph

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,16 +2,17 @@ class PathsController < ApplicationController
   # GET /paths
   # GET /paths.xml
   def index
-	@paths = Issue.order("created_at ASC").limit(40)
+	@paths = Issue.order("created_at ASC").limit(5)
 	@src = 1
 	@g = Graph.new
 
+	### Demo ###
 	if params[:demo] == 'all'
-
+		@g.demo_all()
 	elsif params[:demo] == 'original'
-
+		@g.get_graph_of_earliest()
 	elsif params[:demo] == 'recent'
-
+		@g.get_graph_of_most_recent()
 	end	
   end
 
