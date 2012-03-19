a5944b11d471ef4e7d4cb146add4123fe8718375
Daniel Lobato Garcia <elobatocs at google.com>
2012-03-19 00:17:23 -0700

    Merge comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,9 @@ class Comment < ActiveRecord::Base
 	belongs_to :relationship
 
 	validates :content, :presence => {:message => 'Please enter your comment!'}
+    validates_length_of :content, :within => 1..1000000
 	validates :relationship_id, :presence => true
+    validates_inclusion_of :content_type, :in ['Fixnum'], :message => "Error saving comment"
 
 	has_paper_trail :on=>[:destroy]
 
