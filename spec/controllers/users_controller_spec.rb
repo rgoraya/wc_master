1adc4d9f631cba1b9fa9c0e31e40d17ccb7d98d3
Daniel Lobato Garcia <elobatocs at google.com>
2012-03-26 23:17:12 -0700

    Added stub for show action testing, FactoryGirl for users creation, and a helper for gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it "returns http success" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user    #:user is the one at the controller
+    end      
+  end
+
   describe "GET 'new'" do
     it "returns http success" do
       get :new
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.username              "Dummy"
+  user.email                 "dummyboy@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end       
