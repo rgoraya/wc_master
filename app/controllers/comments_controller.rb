d5bcd305ddf21375ca270ddbb2c4b045f755dda2
Daniel Lobato Garcia <elobatocs at google.com>
2012-04-03 10:41:34 -0700

    Added comments controller spec stub. There is no need for a show action, only create and destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,41 +1,28 @@
 class CommentsController < ApplicationController
-
-  def show
-    @comment = comment.find(params[:id])
+  def create
+    @comment = Comment.new(params[:comment])
+    @relationship = Relationship.find(@comment.relationship_id)
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @comment }
+      if @comment.save
+        format.html { redirect_to(:back, :notice => 'Comment was successfully added.') }
+        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
+        format.js
+      else
+        @notice = @comment.errors
+        format.html { redirect_to(:back, :notice => @comment.errors) }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        format.js
+      end
     end
   end
 
-	def create
-		@comment = Comment.new(params[:comment])
-		
-    @relationship = Relationship.find(@comment.relationship_id)
-    
-    respond_to do |format|
-			if @comment.save
-				format.html { redirect_to(:back, :notice => 'Comment was successfully added.') }
-				format.xml  { render :xml => @comment, :status => :created, :location => @comment }
-				format.js
-			else
-				@notice = @comment.errors
-				format.html { redirect_to(:back, :notice => @comment.errors) }
-				format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-				format.js
-      end
-    end
-	end
-
   def destroy
     @comment = Comment.find(params[:id])
-    
     @relationship = Relationship.find(@comment.relationship_id)
-
     @comment.destroy
 
-		Version.destroy_all(["item_type = ? AND item_id = ?", "Comment", @comment.id])
+    Version.destroy_all(["item_type = ? AND item_id = ?", "Comment", @comment.id])
 
     @comnotice = "Comment Deleted!"
     respond_to do |format|
